ab22b698246f4e2ce8965d8f734337e27794ef19
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-09 14:32:42 -0400

    section 4 folder rename

---
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 4 - beyond fundamentals/section-4-1 - hooks and mounting/app.js
@@ -0,0 +1,10 @@
+let vm = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!"
+    }
+  }
+}) // removing the mount function prevents it from mounting anywhere, but it still exists in js
+
+
+
