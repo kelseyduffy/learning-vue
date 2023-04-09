babea625ed114df1b93854aabc3f9065557021ca
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-08 23:33:36 -0400

    section 2-19

--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 2/section-2-18/app.js
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 2/section-2-18/app.js
@@ -1,2 +1,14 @@
 let vm = Vue.createApp({
+    data() {
+        return {
+            isPurple: false,
+            selectedColor: '',
+            size: 150 // initial value in the CSS class
+        }
+    },
+    computed: {
+        circle_classes() {
+            return { purple: this.isPurple }
+        }
+    }
 }).mount('#app')
