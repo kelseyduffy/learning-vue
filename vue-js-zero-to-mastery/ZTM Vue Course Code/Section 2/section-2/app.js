47ff952707b784966474015bae3f48821f936112
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-08 23:09:26 -0400

    section 2-17

--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 2/section-2/app.js
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 2/section-2/app.js
@@ -4,6 +4,7 @@ const testVar = "test hello"
 // the mount argument takes in a value of where it's going to be mounted. here it's the dev with id of app
 
 const vm = Vue.createApp({ // vm = 'V'ue 'M'odel
+    // define any values (single value, arrays, etc)
     data() {
         return {
             firstName: 'John',
@@ -14,9 +15,13 @@ const vm = Vue.createApp({ // vm = 'V'ue 'M'odel
             age: 20
         }
     },
+    // define any functions needed
     methods: {
         // cleaner to put the logic here in methods to clean up the template
         fullName() {
+            // console log to prove that this is called on any update to the vue instance anywhere (even age)
+            console.log('fullName was called')
+
             return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}` // proxy allows `this` to work (but breaks the use of arrow functions)
         },
         increment() {
@@ -29,11 +34,31 @@ const vm = Vue.createApp({ // vm = 'V'ue 'M'odel
             // event.preventDefault()  
 
             // proceed with custom logic
-            console.log(message)
+            // console.log(message)
             this.lastName = event.target.value
         },
         updateMiddleName(event) {
             this.middleName = event.target.value
+        }
+    },
+    // should only really be used to compute the value of a property. if needing to pass a value, use a method instead (probably)
+    computed: {
+        fullNameAsComputedProp() {
+            // console log to prove that this is only called on relevant updates
+            console.log('fullNameAsComputedProp was called')
+
+            // this.age // adding this reference to age will cause this function to trigger on updates to age as well
+
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}` // proxy allows `this` to work (but breaks the use of arrow functions)
+        }
+    },
+    // properties to watch for
+    watch: {
+        age(newVal, oldVal) {
+            // don't have to return anything and can be async. Vue will update the value regardless after this is called
+            setTimeout(() => {
+                this.age = 20 // revert the age back to 20 after 3 seconds
+            }, 3000)
         }
     }
 }).mount('#app')
