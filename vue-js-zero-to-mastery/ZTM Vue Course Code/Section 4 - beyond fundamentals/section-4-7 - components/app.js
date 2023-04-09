2e943cda10fbbfd582e1272bbb1a01b7732a4ab9
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-09 15:31:19 -0400

    section 4 complete

--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 4 - beyond fundamentals/section-4-7 - components/app.js
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 4 - beyond fundamentals/section-4-7 - components/app.js
@@ -1,9 +1,15 @@
-let vm = Vue.createApp({
+let vm = Vue.createApp({})
+
+// components are kind of similar to mini-instances, but cannot be mounted
+vm.component('hello-world', { // component name can be anything. best practices: namespaced and either kebab or pascal cased
+  template: `<h1>{{ message }}</h1>`,
   data() {
     return {
-      message: "Hello world!"
+      message: 'hello world'
     }
   }
-})
+}) 
 
 vm.mount('#app')
+
+/* Vue dev tools will show the <root> component with three <hello-world> subcomponents now */
