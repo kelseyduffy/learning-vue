9f8ca327f37c4c83e695156674969767501650ec
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-09 12:42:07 -0400

    section 3 complete

--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 3 - quick app/section-3/app.js
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 3 - quick app/section-3/app.js
@@ -1,25 +1,34 @@
 const vm = Vue.createApp({
     data() {
         return {
-            perspective: 0,
+            perspective: 100,
             rotation: [0, 0, 0] // x,y,z
         }
     },
     methods: {
         reset() {
-            this.perspective = 0
+            this.perspective = 100
             this.rotation = [0, 0, 0]
         },
-        copy() {
-            console.log('copy was called.')
+        async copy() {
+            let text = `transform:${this.boxStyle.transform};`
+
+            // docs on clipboard api: https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
+            await navigator.clipboard.writeText(text)
+
+            alert("CSS copied to clipboard")
         }
     },
     computed: {
-        rotationClassValues() {
-            return `rotateX(${this.rotation[0]}deg) rotateY(${this.rotation[1]}deg) rotateZ(${this.rotation[2]}deg)`
-        },
-        boxPerspective() {
-            return `${this.perspective}px`
+        boxStyle() {
+            return {
+                transform: `
+                    perspective(${this.perspective}px)    
+                    rotateX(${this.rotation[0]}deg)
+                    rotateY(${this.rotation[1]}deg)
+                    rotateZ(${this.rotation[2]}deg)
+                `
+            }
         }
     }
 }).mount('#app')
