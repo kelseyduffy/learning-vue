7668bc57c679f9258ac5ed9aab76ee6c4aabeb59
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-12 21:18:26 -0400

    named slots

--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 6 - adv components/slots/src/App.vue
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 6 - adv components/slots/src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
+    <!-- using named slots -->
     <app-form>
-      <form>
-        <div class="help">
-          <p>This is some help text.</p>
-        </div>
-        <div class="fields">
-          <input type="text" placeholder="email">
-          <input type="text" placeholder="username">
-          <input type="password" placeholder="password">
-        </div>
-        <div class="buttons">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      <template v-slot:help>
+        <p>{{ help }}</p>
+      </template>
+      <template v-slot:fields>
+        <input type="text" placeholder="email">
+        <input type="text" placeholder="username">
+        <input type="password" placeholder="password">
+      </template>
+      <template v-slot:buttons>
+        <button type="submit">Submit</button>
+      </template>
+      <p>Dummy text</p> <!-- can use named slots and default slots -->
     </app-form>
+
     <app-form>
+      <div class="help">
+        <p>Contact.</p>
+      </div>
+      <div class="fields">
+        <input type="text" placeholder="email">
+        <input type="text" placeholder="username">
+      </div>
+      <div class="buttons">
+        <button type="submit">Submit</button>
+      </div>
     </app-form>
   </div>
 </template>
@@ -27,6 +38,11 @@ export default {
   name: 'App',
   components: {
     AppForm // naming it 'AppForm' to avoid the naming collision with 'form'
+  },
+  data() {
+    return {
+      help: 'This is some help text.'
+    }
   }
 }
 </script>
--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 6 - adv components/slots/src/components/Form.vue
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 6 - adv components/slots/src/components/Form.vue
@@ -1,5 +1,14 @@
 <template>
   <form>
+      <div class="help">
+        <slot name="help"></slot>
+      </div>
+      <div class="fields">
+        <slot name="fields"></slot>
+      </div>
+      <div class="buttons">
+        <slot name="buttons"></slot>
+      </div>
     <slot> <!-- render content passed in from parent -->
         default content
     </slot>
