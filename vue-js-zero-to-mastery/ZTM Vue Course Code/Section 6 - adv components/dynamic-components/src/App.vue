1ee324e2d18b333af67b65c2846d4314b08263e3
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-12 21:30:14 -0400

    dynamic component switching

--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 6 - adv components/dynamic-components/src/App.vue
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 6 - adv components/dynamic-components/src/App.vue
@@ -1,10 +1,29 @@
 <template>
+  <div>
+    <select v-model="componentName">
+      <option value="Home">Home</option>
+      <option value="About">About</option>
+    </select>
 
+    <!-- use the component component to dynamically set which component is rendering in here -->
+    <component :is="componentName"></component>
+  </div>
 </template>
 
 <script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
 export default {
   name: "App",
+  components: {
+    Home,
+    About,
+  },
+  data() {
+    return {
+      componentName: "Home",
+    }
+  }
 };
 </script>
 
