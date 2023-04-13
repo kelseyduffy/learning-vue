2621a8b83165e36205eee6bfcc5dfa425c5e3a2a
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-12 21:05:29 -0400

    slots form is a child component now

--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 6 - adv components/slots/src/App.vue
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 6 - adv components/slots/src/App.vue
@@ -1,11 +1,15 @@
 <template>
-
+  <app-form />  <!-- you can choose to use kebab-case instead of PascalCase here -->
 </template>
 
 
 <script>
+import AppForm from "./components/Form.vue"
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppForm // naming it 'AppForm' to avoid the naming collision with 'form'
+  }
 }
 </script>
 
