11d6e6e88b7970c20b5efc081b47b8ceccf04aaa
kelseyduffy <kelseyduffy@protonmail.com>
2023-04-16 15:13:46 -0400

    adding animations and transitions

---
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 7 - transitions and animations/notes.md
@@ -0,0 +1,25 @@
+## Transitions
+
+Vue handles the addition and removal of classes during the transition periods
+- it does not do the actual animation itself
+- in the `elements` tab of browser dev tools, you can see the classes being added and removed throughout the animation cycle
+- Vue detects the timing for the animations by looking at the css itself
+    - you can override this by explicitly telling Vue how long the duration is by using `duration=""` in the element in milisecond
+
+element names:
+- `*-enter-from`
+   - called at the very beginning of the element being rendered
+   - removed after the first frame
+   - ex: `fade-enter-from`
+- `*-enter-to`
+   - called at the end when it's disappearing
+   - final frame of the animation, for finishing touches
+- `*-enter-active`
+    - added from the beginning to ending of the animation
+
+they default to `v-*` for each one (`v-enter-from`), but you can add a name to the transition tag (`<transition name="fade">`) to overwrite this (ex: `fade-enter-from`)
+
+transitioning out:
+- everything is the same, but `*-leave-*`
+
+[docs on transitions](https://vuejs.org/guide/built-ins/transition.html)
--- vue-js-zero-to-mastery/ZTM Vue Course Code/Section 7 - transitions and animations/section-7/src/App.vue
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/Section 7 - transitions and animations/section-7/src/App.vue
@@ -1,10 +1,45 @@
-<template></template>
+<template>
+  <div>
+    <button type="button" @click="flag = !flag">Toggle</button>
+
+    <transition name="fade">   <!-- Vue detects how long the animations will be from looking at the css -->
+      <h2 v-if="flag" key="main">Hello world!</h2>
+      <h2 v-else key="secondary">Another hello</h2> <!-- notice how the two transition simulatenously and reposition themselves when theyre done -->
+    </transition>
+    <hr/>
+    <transition name="fade" duration="1000"> <!-- you tell Vue how long the animations will be, in ms. Notice that Vue ripped this element out before its 2.5s animation was done -->
+      <h2 v-show="flag">Hello again</h2>
+    </transition>
+    <hr/>
+    <transition name="fade" mode="out-in">   <!-- Vue detects how long the animations will be from looking at the css -->
+      <h2 v-if="flag" key="main">Hello world!</h2>
+      <h2 v-else key="secondary">Another hello</h2>
+    </transition>
+  </div>
+</template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      flag: false,
+    }
+  }
 };
 </script>
 
 <style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s linear;
+}
+
+.fade-leave-to {
+  transition: all 2.5s linear;
+  opacity: 0;
+}
 </style>
