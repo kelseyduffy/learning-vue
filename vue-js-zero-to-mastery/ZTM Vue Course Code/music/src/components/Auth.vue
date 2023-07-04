a2e27f9e70e4e8f6df2e744624850acbfb210aad
kelseyduffy <kelseyduffy@protonmail.com>
2023-07-04 13:03:11 -0400

    enforce validation before allowing submit button to work

--- vue-js-zero-to-mastery/ZTM Vue Course Code/music/src/components/Auth.vue
+++ vue-js-zero-to-mastery/ZTM Vue Course Code/music/src/components/Auth.vue
@@ -81,7 +81,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -201,6 +201,12 @@ export default {
     ...mapWritableState(useModalStore, {
       modalVisibility: 'isOpen'
     })
+  },
+  methods: {
+    register(values) {
+      console.log('submit button was clicked successfully')
+      console.log(values)
+    }
   }
 }
 </script>
