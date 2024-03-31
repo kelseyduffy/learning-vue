ab264d0684f096d4b55ef2b1f3a0ac1c4326c1ce
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-03-31 13:18:46 -0400

    remove auto generated sections we dont need

--- vue-masterclass/job-search/src/main.js
+++ vue-masterclass/job-search/src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+// could be name '@/App.vue' because vite.config.js defines '@' as 'src' folder
 
+// name "app" just needs to match the name on the div element in index.html
 createApp(App).mount('#app')
