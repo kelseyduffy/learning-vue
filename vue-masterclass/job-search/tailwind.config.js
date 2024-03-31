4c57993e50d3472a7f215eecea09244f8754cb1a
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-03-31 18:54:55 -0400

    add nav bar

--- vue-masterclass/job-search/tailwind.config.js
+++ vue-masterclass/job-search/tailwind.config.js
@@ -6,6 +6,13 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        // creates a custom tailwind class for custom color
+        // automatically creates 'bg-brand-gray-1', 'text-brand-gray-1', etc
+        'brand-gray-1': '#dadce0',
+        'brand-blue-1': '#1967d2',
+        'brand-green-1': '#137333'
       }
     }
   },
