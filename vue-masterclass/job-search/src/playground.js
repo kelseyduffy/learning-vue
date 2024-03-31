9202b08fc120442dfcd130c5d856eca3a8ef0273
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-03-31 19:03:07 -0400

    js object, method, props

--- vue-masterclass/job-search/src/playground.js
+++ vue-masterclass/job-search/src/playground.js
@@ -1,7 +1,18 @@
 // run this file in terminal with 'node /src/playground.js'
 
-const fruits = ['apple', 'banana', 'orange'];
-const vegetables = ['cucumber', 'radish'];
+const developer = {
+  salary: 100000,
+  experience: 4.5,
+  techStack: ['vue', 'html', 'css'],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  }
+};
 
-console.log([fruits, vegetables]); // array of arrays
-console.log([...fruits, ...vegetables]); // merged arrays
+console.log(developer.salary);
+console.log(developer.lookingForWork);
+developer.doubleSalary();
+console.log(developer.salary);
+console.log(developer.lookingForWork);
