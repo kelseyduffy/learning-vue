880065fcc86aa3be2e3e9079b422a6ac8a0e5b3b
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-03-31 18:10:09 -0400

    create new component for the navigation bar

--- vue-masterclass/job-search/src/App.vue
+++ vue-masterclass/job-search/src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1 class="mt-8 text-6xl text-center">Hello Vue</h1>
+  <MainNav />
 </template>
+
+<script>
+import MainNav from '@/components/MainNav.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainNav // because the name matches the alias, you can leave off ': MainNav'
+    //MainNav: MainNav
+  }
+};
+</script>
