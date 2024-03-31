6f037d9ade353aa018bf20ea3292be6af4df6582
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-03-31 15:55:06 -0400

    spread operator

--- vue-masterclass/job-search/src/main.js
+++ vue-masterclass/job-search/src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 
-import '@/index.css'
-import App from './App.vue'
+import '@/index.css';
+import App from './App.vue';
 // could be name '@/App.vue' because vite.config.js defines '@' as 'src' folder
 
 // name "app" just needs to match the name on the div element in index.html
-createApp(App).mount('#app')
+createApp(App).mount('#app');
---
+++ vue-masterclass/job-search/src/playground.js
@@ -0,0 +1,7 @@
+// run this file in terminal with 'node /src/playground.js'
+
+const fruits = ['apple', 'banana', 'orange'];
+const vegetables = ['cucumber', 'radish'];
+
+console.log([fruits, vegetables]); // array of arrays
+console.log([...fruits, ...vegetables]); // merged arrays
