5febbb21e75e2dcfb5c66ce32b3008f7942ead6f
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-05-12 16:04:14 -0400

    more vitest

--- vue-masterclass/job-search/src/playground.js
+++ vue-masterclass/job-search/src/playground.js
@@ -1,18 +1,7 @@
-// run this file in terminal with 'node /src/playground.js'
-
-const developer = {
-  salary: 100000,
-  experience: 4.5,
-  techStack: ['vue', 'html', 'css'],
-  lookingForWork: true,
-  doubleSalary() {
-    this.salary = this.salary * 2;
-    this.lookingForWork = false;
+export const evenOrOdd = (number) => {
+  if (number % 2 === 0) {
+    return 'Even';
+  } else {
+    return 'Odd';
   }
 };
-
-console.log(developer.salary);
-console.log(developer.lookingForWork);
-developer.doubleSalary();
-console.log(developer.salary);
-console.log(developer.lookingForWork);
