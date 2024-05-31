8346f8cc99087aae27f663f5a21ed358bace2d8f
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-05-30 21:28:05 -0400

    add tests and coverage

--- vue-masterclass/job-search/tests/unit/playground.test.js
+++ vue-masterclass/job-search/tests/unit/playground.test.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import { evenOrOdd } from '@/playground';
+import { evenOrOdd, multiply } from '@/playground';
 
 describe('basic math', () => {
   it('adds two numbers', () => {
@@ -18,6 +18,12 @@ describe('basic math', () => {
       it('indicates the number is odd', () => {
         expect(evenOrOdd(3)).toBe('Odd');
       });
+    });
+  });
+
+  describe('multiply', () => {
+    it('multiplies two numbers together', () => {
+      expect(multiply(2, 3)).toBe(6);
     });
   });
 });
