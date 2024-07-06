dfe2d9fd7d32db5dbdbc3dc6615d7db3222dfe21
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-05 22:27:29 -0400

    add menu buttons

--- vue-masterclass/job-search/src/components/MainNav.vue
+++ vue-masterclass/job-search/src/components/MainNav.vue
@@ -2,7 +2,15 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="menuItem in menuItems" :key="menuItem" class="ml-9 h-full first:ml-0">
+              <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -13,7 +21,9 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Bobo Careers'
+      company: 'Bobo Careers',
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at Bobo Corp', 'How we hire', 'Students', 'Jobs']
     };
   }
 };
