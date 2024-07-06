26b8758846c2c53d8b7b68d4af116b1394ef7b75
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-05 20:17:03 -0400

    can override the way the dom is mocked

--- vue-masterclass/job-search/tests/unit/components/MainNav.test.js
+++ vue-masterclass/job-search/tests/unit/components/MainNav.test.js
@@ -4,9 +4,16 @@ import MainNav from '@/components/MainNav.vue';
 
 describe('MainNav', () => {
   it('displays company name', () => {
-    render(MainNav); // mocks a dom with specifically this component
+    render(MainNav, {
+      data() {
+        return {
+          company: 'Super Corp'
+        };
+      }
+    }); // mocks a dom with specifically this component
     //screen.debug(); // can observe the component
-    const companyName = screen.getByText('Bobo Careers');
+    //const companyName = screen.getByText('Bobo Careers');
+    const companyName = screen.getByText('Super Corp');
     console.log(companyName);
     //expect(companyName).toBeInTheDocument();
   });
