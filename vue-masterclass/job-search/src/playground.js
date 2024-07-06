14065e0d7a1751f26c8669e305c8a6c776e7b710
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-06 09:31:55 -0400

    map function

--- vue-masterclass/job-search/src/playground.js
+++ vue-masterclass/job-search/src/playground.js
@@ -1,3 +1,25 @@
+// run this js file directly with `node src/playground.js` in job-serach folder
+
+const numbers = [1, 2, 3, 4, 5];
+const names = ['BOBBY', 'SALLY', 'DEBBIE'];
+
+/* fully written out method 
+const squares = numbers.map((number) => {
+  return number * number;
+}); */
+
+// short hand. one liner lets you remove {}'s and `return`
+const squares = numbers.map((number) => number * number);
+
+const lowercaseNames = names.map((name) => {
+  return name.toLowerCase();
+});
+
+console.log(squares);
+console.log(lowercaseNames);
+
+/* -------------------------------- */
+
 export const evenOrOdd = (number) => {
   if (number % 2 === 0) {
     return 'Even';
