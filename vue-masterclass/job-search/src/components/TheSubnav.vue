e53d8e299ed952737b6d0ad508150b190acdc810
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-08 17:07:26 -0400

    TDD for only showing subnav on jobs page

--- vue-masterclass/job-search/src/components/TheSubnav.vue
+++ vue-masterclass/job-search/src/components/TheSubnav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-16 border-b border-solid border-brand-gray-1 bg-white">
     <div class="flex items-center h-full px-8">
-      <div>
+      <div v-if="onJobResultsPage">
         <!-- providing a class to a component will auto apply it to the top level element in its template -->
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
         <span><span class="text-brand-green-1">1653</span> jobs matched</span>
@@ -12,6 +12,11 @@
 
 <script>
 export default {
-  name: 'TheSubnav'
+  name: 'TheSubnav',
+  data() {
+    return {
+      onJobResultsPage: true
+    };
+  }
 };
 </script>
