260e27e2e5d1ef2289b34df7cb55a517ac489b9f
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-09 19:49:09 -0400

    create hero component

--- vue-masterclass/job-search/src/App.vue
+++ vue-masterclass/job-search/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <MainNav />
+  <main-nav />
+  <the-hero />
 </template>
 
 <script>
 import MainNav from '@/components/MainNav.vue';
+import TheHero from '@/components/TheHero.vue';
 
 export default {
   name: 'App',
   components: {
-    MainNav // because the name matches the alias, you can leave off ': MainNav'
+    MainNav, // because the name matches the alias, you can leave off ': MainNav'
     //MainNav: MainNav
+    TheHero
   }
 };
 </script>
---
+++ vue-masterclass/job-search/src/components/TheHero.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">I can be your hero</section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'TheHero'
+};
+</script>
