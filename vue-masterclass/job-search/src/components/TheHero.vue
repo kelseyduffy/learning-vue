a1225b19bd130399fccfec7cc53a6c3885097e4d
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-09 22:09:35 -0400

    lifecycle hooks

--- vue-masterclass/job-search/src/components/TheHero.vue
+++ vue-masterclass/job-search/src/components/TheHero.vue
@@ -1,11 +1,30 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">I can be your hero</section>
+    <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1 border border-blue-700">1 column</div>
+        <div class="col-start-2 col-span-5 border border-blue-700">5 columns</div>
+        <div class="col-start-7 col-span-5 border border-blue-700">5 columns</div>
+        <div class="col-start-12 col-span-1 border border-blue-700">1 column</div>
+      </div>
+    </section>
   </main>
 </template>
 
 <script>
 export default {
-  name: 'TheHero'
+  name: 'TheHero',
+  beforeCreate() {
+    console.log('Component is about to be created');
+  },
+  created() {
+    console.log('Component has been created');
+  },
+  beforeMount() {
+    console.log('Component is about to be mounted');
+  },
+  mounted() {
+    console.log('Component has been mounted in the DOM');
+  }
 };
 </script>
