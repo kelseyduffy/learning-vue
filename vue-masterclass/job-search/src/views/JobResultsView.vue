31f2df4be4e3264b239e70a38e37134558c01ee8
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-14 18:06:03 -0400

    route and router objects

--- vue-masterclass/job-search/src/views/JobResultsView.vue
+++ vue-masterclass/job-search/src/views/JobResultsView.vue
@@ -1,9 +1,21 @@
 <template>
   <h1 class="text-6xl font-bold">Job Results Page</h1>
+  <h2>{{ routeObject }}</h2>
+  <h2>{{ routerObject }}</h2>
 </template>
 
 <script>
 export default {
-  name: 'JobResultsView'
+  name: 'JobResultsView',
+  computed: {
+    routeObject() {
+      console.log(this.$route);
+      return 'none';
+    },
+    routerObject() {
+      console.log(this.$router);
+      return 'none';
+    }
+  }
 };
 </script>
