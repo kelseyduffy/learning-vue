74b6bb6bf46d617d4a0ca261101baacbd5fa77f9
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-16 18:32:46 -0400

    hard coded job listing example

--- vue-masterclass/job-search/src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
+++ vue-masterclass/job-search/src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Job Filters Sidebar</div>
+  <div class="flex flex-col border-r border-solid border-brand-gray-1 bg-white p-4 w-96">
+    Job Filters Sidebar
+  </div>
 </template>
 
 <script>
