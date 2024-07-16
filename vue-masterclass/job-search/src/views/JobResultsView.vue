65d02b12646779cfa4c0b569c1b06dac12e77c73
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-16 18:20:19 -0400

    second new component

--- vue-masterclass/job-search/src/views/JobResultsView.vue
+++ vue-masterclass/job-search/src/views/JobResultsView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <job-filter-sidebar />
+    <job-listings />
   </div>
 </template>
 
 <script>
 import JobFilterSidebar from '@/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue';
+import JobListings from '@/components/jobResults/JobListings.vue';
 
 export default {
   name: 'JobResultsView',
   components: {
-    JobFilterSidebar
+    JobFilterSidebar,
+    JobListings
   }
 };
 </script>
