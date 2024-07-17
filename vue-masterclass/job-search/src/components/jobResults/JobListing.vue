17e5f6a3b69dc435f9c8b5589d808cf9b80291ce
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-16 23:14:29 -0400

    render actual jobs from json array

--- vue-masterclass/job-search/src/components/jobResults/JobListing.vue
+++ vue-masterclass/job-search/src/components/jobResults/JobListing.vue
@@ -1,19 +1,23 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
     >
       <div class="mx-8 border border-solid border-brand-gray-2 pt-5 pb-2">
-        <h2 class="mb-2 text-2xl">Technical Program Manager, Perception, Augmented Reality</h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
 
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Bobo</span>
+            <span>{{ job.organization }}</span>
           </div>
 
           <div>
-            <span>San Francisco, CA, USA</span>
+            <ul>
+              <li v-for="location in job.locations" :key="location" class="inline-block mr-5">
+                <span>{{ location }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -23,15 +27,15 @@
           <h3 class="mt-1 mb-2">Qualifications:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>Bachelor's degree</li>
-              <li>5 years experience</li>
-              <li>Experience analyzing data through querying database (eg. SQL)</li>
+              <li v-for="qualification in job.minimumQualifications" :key="qualification">
+                {{ qualification }}
+              </li>
             </ul>
           </div>
         </div>
 
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1">Expand</router-link>
+          <router-link :to="jobPageLink" class="text-brand-blue-1">Expand</router-link>
         </div>
       </div>
     </router-link>
@@ -40,6 +44,17 @@
 
 <script>
 export default {
-  name: 'JobListing'
+  name: 'JobListing',
+  props: {
+    job: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    }
+  }
 };
 </script>
