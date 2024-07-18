3d6aa7be917918b5c7cfd45e9f6828821b5deb2f
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-17 22:54:18 -0400

    job listings tests with async get data load

--- vue-masterclass/job-search/tests/unit/components/jobResults/JobListings.test.js
+++ vue-masterclass/job-search/tests/unit/components/jobResults/JobListings.test.js
@@ -1,13 +1,35 @@
 import { render, screen } from '@testing-library/vue';
+import { RouterLinkStub } from '@vue/test-utils';
 import axios from 'axios';
 
 import JobListings from '@/components/JobResults/JobListings.vue';
 
 vi.mock('axios');
-// console.log(axios); // console log shows the difference between normal axios and mock axios object
+//console.log(axios); // console log shows the difference between normal axios and mock axios object
 
 describe('JobListings', () => {
-  it('renders', () => {
-    expect(1).toBe(1);
+  const renderJobListings = () => {
+    render(JobListings, {
+      global: {
+        stubs: {
+          'router-link': RouterLinkStub
+        }
+      }
+    });
+  };
+  it('fetches jobs', () => {
+    axios.get.mockResolvedValue({ data: [] });
+    renderJobListings();
+
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs');
+  });
+
+  it('creates a job listing for every job', async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    renderJobListings();
+
+    // get<action> is synchronous, find<action> is async
+    const jobListings = await screen.findAllByRole('listitem');
+    expect(jobListings).toHaveLength(15);
   });
 });
