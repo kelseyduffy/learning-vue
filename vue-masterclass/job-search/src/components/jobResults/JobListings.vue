2d2695c0dbaf1ff90a929b453bee45bd9080df6f
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-19 19:42:55 -0400

    render page number and page buttons

--- vue-masterclass/job-search/src/components/jobResults/JobListings.vue
+++ vue-masterclass/job-search/src/components/jobResults/JobListings.vue
@@ -3,6 +3,28 @@
     <ol>
       <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+
+    <div class="mx-auto mt-8">
+      <div class="flex flex-row flex-nowrap">
+        <p class="flex flex-grow text-sm">Page {{ currentPage }}</p>
+
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Previous</router-link
+          >
+
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Next</router-link
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -21,12 +43,24 @@ export default {
     };
   },
   computed: {
+    currentPage() {
+      return Number.parseInt(this.$route.query.page || '1');
+    },
+    previousPage() {
+      const previousPage = this.currentPage - 1;
+      return previousPage >= 1 ? previousPage : undefined;
+    },
+    maxPage() {
+      return Math.ceil(this.jobs.length / 10);
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      return nextPage <= this.maxPage ? nextPage : undefined;
+    },
     displayedJobs() {
-      const pageString = this.$route.query.page || '1'; // fall back to page 1 if not specified in route
-      const pageNumber = Number.parseInt(pageString);
       const pageSize = 10;
-      const firstJobIndex = (pageNumber - 1) * pageSize;
-      const lastJobIndex = pageNumber * pageSize;
+      const firstJobIndex = (this.currentPage - 1) * pageSize;
+      const lastJobIndex = this.currentPage * pageSize;
       return this.jobs.slice(firstJobIndex, lastJobIndex);
     }
   },
