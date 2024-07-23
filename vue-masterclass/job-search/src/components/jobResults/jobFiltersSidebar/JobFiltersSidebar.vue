58bb060f0b6e3e572e5ba238b491e9cf0ac5f108
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-23 18:05:55 -0400

    basic accordion component

--- vue-masterclass/job-search/src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
+++ vue-masterclass/job-search/src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="flex flex-col border-r border-solid border-brand-gray-1 bg-white p-4 w-96">
-    Job Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+
+      <collapsible-accordion />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from '@/components/shared/ActionButton.vue';
+import CollapsibleAccordion from '@/components/shared/CollapsibleAccordion.vue';
+
 export default {
-  name: 'JobFiltersSidebar'
+  name: 'JobFiltersSidebar',
+  components: { ActionButton, CollapsibleAccordion }
 };
 </script>
--- vue-masterclass/job-search/src/main.js
+++ vue-masterclass/job-search/src/main.js
@@ -2,13 +2,15 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleUp, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import '@/index.css';
 import router from '@/router'; // It is the default export so we don't need { }'s
 import App from './App.vue';
 // could be name '@/App.vue' because vite.config.js defines '@' as 'src' folder
 
+library.add(faAngleDown);
+library.add(faAngleUp);
 // add the search maginifying glass to the library of available icons
 library.add(faSearch);
 
