2e8bfaf40051009171e15047dcaf77b4d29e3d44
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-23 18:40:15 -0400

    Update CollapsibleAccordion.test.js

--- vue-masterclass/job-search/tests/unit/components/shared/CollapsibleAccordion.test.js
+++ vue-masterclass/job-search/tests/unit/components/shared/CollapsibleAccordion.test.js
@@ -5,7 +5,7 @@ import CollapsibleAccordion from '@/components/shared/CollapsibleAccordion.vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 describe('CollapsibleAccordion', () => {
-  it('renders child content', async () => {
+  const renderCollapsibleAccordion = (config = {}) => {
     render(CollapsibleAccordion, {
       global: {
         stubs: {
@@ -17,8 +17,21 @@ describe('CollapsibleAccordion', () => {
       },
       slots: {
         default: '<h3>My nested child</h3>'
-      }
+      },
+      ...config
     });
+  };
+
+  it('renders child content', async () => {
+    const props = {
+      header: 'My Category'
+    };
+    const slots = {
+      default: '<h3>My nested child</h3>'
+    };
+    const config = { props, slots };
+
+    renderCollapsibleAccordion(config);
 
     expect(screen.queryByText('My nested child')).not.toBeInTheDocument();
 
@@ -32,16 +45,13 @@ describe('CollapsibleAccordion', () => {
 
   describe('when parent does not provide custom child content', () => {
     it('renders default content', async () => {
-      render(CollapsibleAccordion, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true
-          }
-        },
-        props: {
-          header: 'My Category'
-        }
-      });
+      const props = {
+        header: 'My Category'
+      };
+      const slots = {};
+      const config = { props, slots };
+
+      renderCollapsibleAccordion(config);
 
       const button = screen.getByRole('button', {
         name: /my category/i
