a7f57bce49874529338a6699c3c0c990c0f2d870
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-22 22:52:04 -0400

    implement jobs store

--- vue-masterclass/job-search/tests/unit/api/getJobs.test.js
+++ vue-masterclass/job-search/tests/unit/api/getJobs.test.js
@@ -6,24 +6,24 @@ vi.mock('axios');
 
 describe('getJobs', () => {
   beforeEach(() => {
-    axios.get.mockResolvedValue = {
+    axios.get.mockResolvedValue({
       data: [
         {
           id: 1,
           title: 'Java Engineer'
         }
       ]
-    };
+    });
   });
   it('fetches jobs that candidates can apply to', async () => {
-    const url = 'http://myfakeapi.com:3000/jobs';
+    const url = 'http://myfakeapi.com/jobs';
 
     await getJobs();
     expect(axios.get).toHaveBeenCalledWith(url);
   });
 
   it('extracts jobs from response', async () => {
-    const data = await getJobs();
-    expect(data).toEqual([{ id: 1, title: 'Java Engineer' }]);
+    const jobs = await getJobs();
+    expect(jobs).toEqual([{ id: 1, title: 'Java Engineer' }]);
   });
 });
