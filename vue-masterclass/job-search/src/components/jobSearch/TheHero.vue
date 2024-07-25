84a42b1ae204c72873e889d548165c1ae9652ad0
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-07-25 19:01:28 -0400

    render the images using spotlight

--- vue-masterclass/job-search/src/components/jobSearch/TheHero.vue
+++ vue-masterclass/job-search/src/components/jobSearch/TheHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex flex-col h-screen pt-10 pb-20">
+  <main class="flex flex-col h-screen">
+    <section class="pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-start-1 col-span-1">1 column</div>
         <div class="col-start-2 col-span-5">
@@ -16,18 +16,44 @@
         <div class="col-start-12 col-span-1">1 column</div>
       </div>
     </section>
+
+    <spot-light class="flex flex-row justify-center pb-16">
+      <template #default="slotProps">
+        <router-link
+          to="/jobs/results"
+          class="mx-5 flex flex-col h-96 w-72 rounded-lg border bg-brand-gray-2"
+        >
+          <img :src="slotProps.spotlight.img" class="object-contain" />
+
+          <div class="mt-3 h-48 px-6 py-4">
+            <h3 class="text-lg font-medium">
+              {{ slotProps.spotlight.title }}
+            </h3>
+
+            <p class="mt-3 text-sm">
+              {{ slotProps.spotlight.description }}
+            </p>
+          </div>
+          <router-link to="/jobs/results" class="px-6 pb-4 text-sm text-brand-blue-1"
+            >See jobs</router-link
+          >
+        </router-link>
+      </template>
+    </spot-light>
   </main>
 </template>
 
 <script>
 import TheHeadline from '@/components/jobSearch/TheHeadline.vue';
 import JobSearchForm from '@/components/jobSearch/JobSearchForm.vue';
+import SpotLight from '@/components/jobSearch/SpotLight.vue';
 
 export default {
   name: 'TheHero',
   components: {
     TheHeadline,
-    JobSearchForm
+    JobSearchForm,
+    SpotLight
   }
 };
 </script>
