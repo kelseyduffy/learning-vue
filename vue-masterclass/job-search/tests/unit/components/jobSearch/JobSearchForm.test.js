d0516da9eed5e4e6dfb1ad2385075abeb10b8308
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-08-04 17:26:09 -0400

    useRouter composable

--- vue-masterclass/job-search/tests/unit/components/jobSearch/JobSearchForm.test.js
+++ vue-masterclass/job-search/tests/unit/components/jobSearch/JobSearchForm.test.js
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/vue';
 import userEvent from '@testing-library/user-event';
 
+import { useRouter } from 'vue-router';
+vi.mock('vue-router');
+
 import JobSearchForm from '@/components/jobSearch/JobSearchForm.vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
@@ -8,13 +11,10 @@ describe('JobSearchForm', () => {
   describe('when user submits form', () => {
     it("directs user to job results page with user's search parameters", async () => {
       const push = vi.fn();
-      const $router = { push };
+      useRouter.mockReturnValue({ push });
 
       render(JobSearchForm, {
         global: {
-          mocks: {
-            $router
-          },
           stubs: {
             FontAwesomeIcon: true
           }
