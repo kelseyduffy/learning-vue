893e6da45432072e435a4434307ad8e693c8e96c
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-08-13 22:43:17 -0400

    refactor organization sidebar to composition api

--- vue-masterclass/job-search/tests/unit/components/jobResults/jobFiltersSidebar/JobFiltersSidebarOrganization.test.js
+++ vue-masterclass/job-search/tests/unit/components/jobResults/jobFiltersSidebar/JobFiltersSidebarOrganization.test.js
@@ -1,24 +1,21 @@
 import { render, screen } from '@testing-library/vue';
 import userEvent from '@testing-library/user-event';
-import { CreatePinia, createTestingPinia } from '@pinia/testing';
+import { createTestingPinia } from '@pinia/testing';
+import { useRouter } from 'vue-router';
+vi.mock('vue-router');
 
-import JobFiltersSidebarOrganizations from '@/components/JobResults/jobFiltersSidebar/JobFiltersSidebarOrganizations.vue';
+import JobFiltersSidebarOrganizations from '@/components/jobResults/jobFiltersSidebar/JobFiltersSidebarOrganizations.vue';
 import { useJobsStore } from '@/stores/jobs';
 import { useUserStore } from '@/stores/user';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 describe('JobFiltersSidebarOrganizations', () => {
   const renderJobFiltersSidebarOrganizations = () => {
     const pinia = createTestingPinia();
     const userStore = useUserStore();
     const jobsStore = useJobsStore();
-    const $router = { push: vi.fn() };
 
     render(JobFiltersSidebarOrganizations, {
       global: {
-        mocks: {
-          $router
-        },
         plugins: [pinia],
         stubs: {
           FontAwesomeIcon: true
@@ -26,7 +23,7 @@ describe('JobFiltersSidebarOrganizations', () => {
       }
     });
 
-    return { jobsStore, userStore, $router };
+    return { jobsStore, userStore };
   };
   it('renders unique list of organizations from jobs', async () => {
     const { jobsStore } = renderJobFiltersSidebarOrganizations();
@@ -41,6 +38,8 @@ describe('JobFiltersSidebarOrganizations', () => {
   });
   describe('when user clicks checkbox', () => {
     it('communicates that user has selected checkbox for organization', async () => {
+      useRouter.mockReturnValue({ push: vi.fn() });
+
       const { jobsStore, userStore } = renderJobFiltersSidebarOrganizations();
       jobsStore.UNIQUE_ORGANIZATIONS = new Set(['Google', 'Microsoft']);
 
@@ -56,7 +55,9 @@ describe('JobFiltersSidebarOrganizations', () => {
     });
 
     it('communicates that user has selected checkbox for organization', async () => {
-      const { jobsStore, $router } = renderJobFiltersSidebarOrganizations();
+      const push = vi.fn();
+      useRouter.mockReturnValue({ push });
+      const { jobsStore } = renderJobFiltersSidebarOrganizations();
       jobsStore.UNIQUE_ORGANIZATIONS = new Set(['Google']);
 
       const button = screen.getByRole('button', { name: /organizations/i });
@@ -67,7 +68,7 @@ describe('JobFiltersSidebarOrganizations', () => {
       });
       await userEvent.click(googleCheckbox);
 
-      expect($router.push).toHaveBeenCalledWith({ name: 'JobResults' });
+      expect(push).toHaveBeenCalledWith({ name: 'JobResults' });
     });
   });
 });
