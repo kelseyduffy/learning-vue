e5d42955e16e7bcb397bf5d1808005e6d4c3b969
Kelsey Duffy <kelseyduffy@protonmail.com>
2024-09-28 15:32:22 -0400

    split job filters component into sub component

--- vue-masterclass/job-search/src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
+++ vue-masterclass/job-search/src/components/jobResults/jobFiltersSidebar/JobFiltersSidebar.vue
@@ -1,58 +1,27 @@
 <template>
   <div class="flex flex-col border-r border-solid border-brand-gray-1 bg-white p-4 w-96">
     <section class="pb-5">
-      <div class="flex flex-row justify-between">
-        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
-        <div class="flex items-center text-sm">
-          <action-button
-            text="Clear Filters"
-            type="secondary"
-            @click="userStore.CLEAR_USER_JOB_FILTER_SELECTIONS"
-          />
-        </div>
-      </div>
+      <job-filters-sidebar-prompt />
 
       <collapsible-accordion header="Degrees">
-        <job-filters-sidebar-checkbox-group
-          :unique-values="UNIQUE_DEGREES"
-          :action="userStore.ADD_SELECTED_DEGREES"
-        />
+        <job-filters-sidebar-degrees />
       </collapsible-accordion>
 
       <collapsible-accordion header="Job Types">
-        <job-filters-sidebar-checkbox-group
-          :unique-values="UNIQUE_JOB_TYPES"
-          :action="userStore.ADD_SELECTED_JOB_TYPES"
-        />
+        <job-filters-sidebar-job-types />
       </collapsible-accordion>
 
       <collapsible-accordion header="Organization">
-        <job-filters-sidebar-checkbox-group
-          :unique-values="UNIQUE_ORGANIZATIONS"
-          :action="userStore.ADD_SELECTED_ORGANIZATIONS"
-        />
+        <job-filters-sidebar-organizations />
       </collapsible-accordion>
     </section>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
-
-import ActionButton from '@/components/shared/ActionButton.vue';
 import CollapsibleAccordion from '@/components/shared/CollapsibleAccordion.vue';
-import JobFiltersSidebarCheckboxGroup from '@/components/jobResults/jobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue';
-
-import { useDegreesStore } from '@/stores/degrees';
-import { useJobsStore } from '@/stores/jobs';
-import { useUserStore } from '@/stores/user';
-
-const degreesStore = useDegreesStore();
-const UNIQUE_DEGREES = computed(() => degreesStore.UNIQUE_DEGREES);
-
-const jobsStore = useJobsStore();
-const UNIQUE_ORGANIZATIONS = computed(() => jobsStore.UNIQUE_ORGANIZATIONS);
-const UNIQUE_JOB_TYPES = computed(() => jobsStore.UNIQUE_JOB_TYPES);
-
-const userStore = useUserStore();
+import JobFiltersSidebarDegrees from '@/components/jobResults/jobFiltersSidebar/JobFiltersSidebarDegrees.vue';
+import JobFiltersSidebarJobTypes from '@/components/jobResults/jobFiltersSidebar/JobFiltersSidebarJobTypes.vue';
+import JobFiltersSidebarOrganizations from '@/components/jobResults/jobFiltersSidebar/JobFiltersSidebarOrganizations.vue';
+import JobFiltersSidebarPrompt from '@/components/jobResults/jobFiltersSidebar/JobFiltersSidebarPrompt.vue';
 </script>
